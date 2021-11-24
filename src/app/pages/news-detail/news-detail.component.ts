5e887b791ed60ad921119ebb8e4b1e3f2025ed4d
Yeison Osorio <y.osorioatehortua@dxc.com>
2021-11-24 13:24:44 -0500

    Feat(newsDetail): se agrega rutas y componente de detalles

--- src/app/pages/news-detail/news-detail.component.ts
+++ src/app/pages/news-detail/news-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InewsResponse } from 'src/app/models/interfaces';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-news-detail',
@@ -11,6 +12,7 @@ export class NewsDetailComponent implements OnInit {
   idNews: number = 0
   data: null | string = null
   news: Array<InewsResponse> = [];
+  newsFilter: Array<InewsResponse> = [];
   newsDetail: InewsResponse | undefined;
   
   constructor(route: ActivatedRoute) {
@@ -27,6 +29,33 @@ export class NewsDetailComponent implements OnInit {
     this.newsDetail = this.news.find(item => this.idNews == item.id)
     console.log(this.newsDetail)
 
+    this.deleteNews()
+
+  }
+
+  async deleteNews(): Promise<void> {
+
+    this.data = sessionStorage.getItem("news");
+    this.news = this.data != null ? JSON.parse(this.data) : '' ;
+    this.newsFilter = await this.news.filter(item => this.idNews != item.id)
+   
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
   }
 
 }
--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Payload, InewsResponse } from '../../models/interfaces';
 import { NewsService } from '../../services/news.service';
-
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
@@ -14,11 +13,12 @@ export class NewsComponent implements OnInit, OnDestroy, Payload {
   constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
+    
     this.data = sessionStorage.getItem("news");
 
     console.log(this.data)
     const newsSession = this.data != null ? JSON.parse(this.data) : '' ;
-      this.news = newsSession
+    this.news = newsSession
 
     //Solo se solicitan los datos a la API si esta vacio el sessionStorage
     // if (newsSession == '') {
